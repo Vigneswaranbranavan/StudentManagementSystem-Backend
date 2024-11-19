bc767fda03679813ce6d9a937c5326586afb70b4
ut01265 <ut01265@UNICOMTIC24>
2024-11-19 10:52:20 +0530

    Implement Staff

--- StudentManagementSystem/Controllers/StaffController.cs
+++ StudentManagementSystem/Controllers/StaffController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using StudentManagementSystem.DTO.Request;
+using StudentManagementSystem.IServices;
+using StudentManagementSystem.Services;
 
 namespace StudentManagementSystem.Controllers
 {
@@ -7,5 +11,125 @@ namespace StudentManagementSystem.Controllers
     [ApiController]
     public class StaffController : ControllerBase
     {
+        private readonly IStaffService _staffService;
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpPost("Staff")]
+        public async Task<IActionResult> AddStaff(StaffRequest request)
+        {
+            try
+            {
+                var ReturnData = await _staffService.AddStaff(request);
+                return Ok(ReturnData);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpGet("Staff")]
+        public async Task<IActionResult> GetStaff()
+        {
+            try
+            {
+                var data = await _staffService.GetStaff();
+                return Ok(data);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+
+        [HttpGet("StaffById")]
+        public async Task<IActionResult> GetStaffById(Guid id)
+        {
+            try
+            {
+                var data = await _staffService.GetStaffById(id);
+                return Ok(data);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("Staff")]
+        public async Task<IActionResult> UpdateStaff(Guid Id, StaffRequest request)
+        {
+            try
+            {
+                var data = await _staffService.UpdateStaff(Id, request);
+                return Ok(data);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpDelete("Staff")]
+        public async Task<IActionResult> DeleteStaff(Guid id)
+        {
+            try
+            {
+                var data = await _staffService.DeleteStaff(id);
+                return Ok(data);
+            }
+            catch (SqlException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
--- StudentManagementSystem/Repository/StaffRepository.cs
+++ StudentManagementSystem/Repository/StaffRepository.cs
@@ -1,6 +1,74 @@
-﻿namespace StudentManagementSystem.Repository
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.DTO.Request;
+using StudentManagementSystem.Entities;
+using StudentManagementSystem.IRepository;
+
+namespace StudentManagementSystem.Repository
 {
-    public class StaffRepository
+    public class StaffRepository : IStaffRepository
     {
+        private readonly AppDbContext _appDbContext;
+        public StaffRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Staff> AddStaff(Staff Staff)
+        {
+            await _appDbContext.Staff.AddAsync(Staff);
+            await _appDbContext.SaveChangesAsync();
+            return Staff;
+        }
+
+
+        public async Task<List<Staff>> GetStaff()
+        {
+            var StaffData = await _appDbContext.Staff.ToListAsync();
+            return StaffData;
+
+        }
+
+        public async Task<Staff> GetStaffById(Guid id)
+        {
+            var StaffData = await _appDbContext.Staff.FindAsync(id);
+            if (StaffData == null)
+            {
+                throw new Exception("ID is Not Found");
+            }
+
+            return StaffData;
+
+        }
+
+        public async Task<Staff> UpdateStaff(Guid id, StaffRequest request)
+        {
+            var StaffData = await _appDbContext.Staff.FindAsync(id);
+            if (StaffData == null)
+            {
+                throw new Exception("ID is Not Found");
+            }
+
+            StaffData.Name = request.Name;
+            StaffData.Email = request.Email;
+            StaffData.Phone = request.Phone;
+            StaffData.Department = request.Department;
+
+
+            _appDbContext.SaveChanges();
+            return StaffData;
+        }
+
+        public async Task<Staff> DeleteStaff(Guid id)
+        {
+            var StaffData = await _appDbContext.Staff.FindAsync(id);
+            if (StaffData == null)
+            {
+                throw new Exception("ID is Not Found");
+            }
+
+            _appDbContext.Remove(StaffData);
+            _appDbContext.SaveChanges();
+            return StaffData;
+        }
     }
 }
